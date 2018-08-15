ee2dbee82e755967a78acfcc9063ebecf2a2b952
DrawGun <drawgun@yandex.ru>
2018-08-15 09:25:23 +0300

    Routes

---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+import ProductsRoute from 'routes/Products';
+import ProductRoute from 'routes/Product';
+import BascketRoute from 'routes/Basket';
+import ContactsRoute from 'routes/Contacts';
+
+export default () => ([
+  ProductsRoute,
+  ProductRoute,
+  BascketRoute,
+  ContactsRoute
+]);
